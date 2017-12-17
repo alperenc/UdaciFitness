9752f7814f4119fb15979d21069e7b94ed9424f6
Alp Eren Can <alperenc@gmail.com>
2017-12-17 17:16:28 +0300

    Build Steppers with TouchableHighlight from RN

--- components/Steppers.js
+++ components/Steppers.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableHighlight } from 'react-native';
+import { FontAwesome, Entypo } from '@expo/vector-icons';
 
-const Steppers = () => {
+const Steppers = ({ max, unit, step, value, onIncrement, onDecrement }) => {
   return (
     <View>
-      <Text>Steppers</Text>
+      <View>
+        <TouchableHighlight onPress={onDecrement} underlayColor="#0f0">
+          <FontAwesome name="minus" size={30} color={'black'} />
+        </TouchableHighlight>
+        <TouchableHighlight onPress={onIncrement} underlayColor="#0f0">
+          <FontAwesome name="plus" size={30} color={'black'} />
+        </TouchableHighlight>
+      </View>
+      <View>
+        <Text>{value}</Text>
+        <Text>{unit}</Text>
+      </View>
     </View>
   );
 };
