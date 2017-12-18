4c8889135794b99b9f0af427e4b3b705d2a8a331
Alp Eren Can <alperenc@gmail.com>
2017-12-18 12:46:10 +0300

    Export entries()

--- reducers/index.js
+++ reducers/index.js
@@ -1,21 +1,23 @@
 import { RECEIVE_ENTRIES, ADD_ENTRY } from '../actions';
 
-const entries = (state = {}, action) {
+const entries = (state = {}, action) => {
   switch (action.type) {
     case RECEIVE_ENTRIES:
       return {
         ...state,
         ...action.entries,
-      }
+      };
       break;
     case ADD_ENTRY:
       return {
         ...state,
         ...action.entry,
-      }
+      };
       break;
 
     default:
       break;
   }
-}
+};
+
+export default entries;
