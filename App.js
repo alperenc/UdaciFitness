90299286a29c7aeb8311662ba41ac3be601dd3e3
Alp Eren Can <alperenc@gmail.com>
2017-12-24 15:32:44 +0300

    Add EntryDetail and use it in History, ultimately within StackNavigator

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { View, Platform, StatusBar } from 'react-native';
-import { TabNavigator } from 'react-navigation';
+import { TabNavigator, StackNavigator } from 'react-navigation';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { Constants } from 'expo';
 import { FontAwesome, Ionicons } from '@expo/vector-icons';
 import AddEntry from './components/AddEntry';
 import History from './components/History';
+import EntryDetail from './components/EntryDetail';
 import { purple, white } from './utils/colors';
 import reducer from './reducers';
 
@@ -58,13 +59,28 @@ const Tabs = TabNavigator(
   }
 );
 
+const MainNavigator = StackNavigator({
+  Home: {
+    screen: Tabs,
+  },
+  EntryDetail: {
+    screen: EntryDetail,
+    navigationOptions: {
+      headerTintColor: white,
+      headerStyle: {
+        backgroundColor: purple,
+      },
+    },
+  },
+});
+
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
         <View style={{ flex: 1 }}>
           <UdaciStatusBar backgroundColor={purple} barStyle="light-content" />
-          <Tabs />
+          <MainNavigator />
         </View>
       </Provider>
     );
